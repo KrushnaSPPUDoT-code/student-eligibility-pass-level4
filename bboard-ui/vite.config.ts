0d8e399a8ac8fc4d731a1eb78a580a00a0ef2b3c
KrushnaSPPUDoT-code <krushnasppu@gmail.com>
2026-09-07 18:03:49 +0000

    chore: configure frontend for preprod deployment

--- bboard-ui/vite.config.ts
+++ bboard-ui/vite.config.ts
@@ -21,7 +21,7 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/student-eligibility-pass-level3/',
+  base: '/student-eligibility-pass-level4/',
   cacheDir: './.vite',
   build: {
     target: 'esnext',
